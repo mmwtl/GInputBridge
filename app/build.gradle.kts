99167a2216341d976689d65a9a5d2255da43b5cf
wital <mmwtl@yandex.ru>
2026-08-08 00:04:32 +0300

    Bump version to 4.4.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
 //        applicationId = "com.ecarx.hardkeytest"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1635
-        versionName = "4.4.5"
+        versionCode = 1636
+        versionName = "4.4.6"
 
         setProperty("archivesBaseName", "$versionName[$versionCode]GInputBridge")
 
